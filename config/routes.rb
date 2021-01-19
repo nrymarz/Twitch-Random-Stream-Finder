bf2fe04f6f3bc23c95177476d77b4950e530c7ec
nrymarz <nrymarz@gmail.com>
2021-01-19 00:08:55 -0600

    created routed and stubbed controller actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :users
+  resources :users do 
+    resources :recommendations, only: [:show,:index]
+  end
+  resources :recommendations 
+  resources :channels, only: [:show]
   root to: 'static#home'
   match '/auth/:provider/callback', to: 'sessions#create', via: [:get, :post] 
 end
