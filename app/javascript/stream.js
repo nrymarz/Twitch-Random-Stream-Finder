df4bc2d23de0fbe7851ea564645e1bc73d8e443e
Nathan Rymarz <nrymarz@gmail.com>
2021-03-31 17:46:54 -0500

    removed unused js file

--- app/javascript/stream.js
+++
@@ -1,7 +0,0 @@
-const streamPlayer = document.querySelector("iframe")
-const configObj = {
-    headers: {
-        'Authorization': `Bearer `
-    }
-}
-fetch("https://api.twitch.tv/helix/search/channels")
