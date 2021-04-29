b9962a399a8b41eeafb7f6b918bde39a820bc0a8
Nathan Rymarz <nrymarz@gmail.com>
2021-04-29 15:33:56 -0500

    cors stuff

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins {'https://randomstreamfinder.herokuapp.com',"https://randomstreamfinder.herokuapp.com/#"}
+      origins ['https://randomstreamfinder.herokuapp.com',"https://randomstreamfinder.herokuapp.com/#"]
   
       resource '*',
         headers: :any,
